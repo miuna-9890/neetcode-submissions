7374d96aa4a8e79a60063bac5e71007ff39da83f
miuna-9890 <10mithuna@gmail.com>
2026-05-04 12:37:01 +0800

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+
+    int[][] arr = new int[position.length][2];
+
+    for (int i = 0; i < position.length; i++) {
+        arr[i][0] = position[i];
+        arr[i][1] = speed[i];
+    }
+
+    Arrays.sort(arr, (a,b) -> a[0] - b[0]);
+
+    int fleet = 1;
+    double slowest = (double) (target - arr[arr.length - 1][0])/arr[arr.length - 1][1];
+    for (int i = arr.length - 2; i >= 0; i--) {
+        if ((double) (target - arr[i][0])/arr[i][1] > slowest) {
+            fleet+=1;
+            slowest = (double) (target - arr[i][0])/arr[i][1];
+        }
+    }
+    return fleet;
+    }
+}
