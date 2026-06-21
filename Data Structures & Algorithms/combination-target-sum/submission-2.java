5ae1a1a4f8766108b322633f9bbcf1e5804cea64
miuna-9890 <10mithuna@gmail.com>
2026-06-21 20:09:05 +0800

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+
+        List<Integer> sub = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+
+        dfs(nums, target, 0, sub, ans);
+        return ans;
+    }
+
+    public void dfs(int[] nums, int target, int start, List<Integer> sub, List<List<Integer>> ans) {
+        if (target == 0) {
+            ans.add(new ArrayList<>(sub));
+        } else if (target < 0) {
+            return;
+        }
+
+        for (int i = start; i < nums.length; i++) {
+        sub.add(nums[i]);
+        dfs(nums, target - nums[i], i, sub, ans);
+        sub.remove(sub.size() - 1);
+        }
+    }
+}
